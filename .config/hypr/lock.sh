e347548fbc2ecc66c99d76b688a87bf9e2e2d7ad
wefeng97 <weijie.feng@physik.uzh.ch>
2024-09-28 23:49:42 +0200

    swaylock --grace doesnt work. Switched off.

--- .config/hypr/lock.sh
+++ .config/hypr/lock.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 
-timegrace=$2
 timefade=$1
+timegrace=$2
 
-swaylock -e -i /home/wefeng/Pictures/My_Wallpapers/SpiritedAway/wp8575978.png --clock --fade-in $timefade --inside-color ffe5bf --ring-color 62b9e9 -n --key-hl-color ffe5bf --separator-color ffe5bf --grace $timegrace --indicator-idle-visible --font "ComicShanns mono nerd font" --text-color 62b9e9 --indicator-x-position 100 --indicator-y-position 100
+swaylock --clock -e -i /home/wefeng/Pictures/My_Wallpapers/SpiritedAway/wp8575978.png \
+  --inside-color ffe5bf --ring-color 62b9e9 -n \
+  --key-hl-color ffe5bf --separator-color ffe5bf \
+  --font "ComicShanns mono nerd font" \
+  --text-color 62b9e9 --indicator-x-position 100 --indicator-y-position 100 \
+  --fade-in $timefade \
+  --indicator-idle-visible #--grace $timegrace 
   
