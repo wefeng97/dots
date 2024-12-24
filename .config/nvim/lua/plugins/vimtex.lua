1ea1182965fbec3f89a9d30aa799e6918cd3b4ac
wefeng97 <weijie.feng@physik.uzh.ch>
2024-12-24 13:00:29 +0100

    Work

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     config = function ()
         vim.g.vimtex_view_general_viewer = 'okular'
-       vim.g.vimtex_compiler_latexmk_engines = { _ = '-xelatex'}
+       vim.g.vimtex_compiler_latexmk_engines = { _ = '-pdflatex'}
         vim.g.tex_comment_nospell = 1
         vim.g.vimtex_compiler_progname = 'nvr'
         vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
