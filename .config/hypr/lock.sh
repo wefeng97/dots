88bdf13738948d1f94184aa22dfd24f06ac35305
wefeng97 <weijie.feng@physik.uzh.ch>
2025-06-07 21:52:53 +0200

    update 2025-06-07

--- .config/hypr/lock.sh
+++ .config/hypr/lock.sh
@@ -3,7 +3,7 @@
 timefade=$1
 timegrace=$2
 
-swaylock --clock -i /home/wefeng/Pictures/My_Wallpapers/SpiritedAway/wp8575978.png \
+/usr/bin/swaylock --clock -i /home/wefeng/Pictures/My_Wallpapers/SpiritedAway/wp8575978.png \
   --inside-color ffe5bf --ring-color 62b9e9 -n \
   --key-hl-color ffe5bf --separator-color ffe5bf \
   --font "ComicShanns mono nerd font" \
